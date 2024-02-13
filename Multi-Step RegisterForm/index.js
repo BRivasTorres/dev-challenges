db72a7cf85fc40b3507e452ddb0e9760ff335660
Billy Torres <btorresr75@gmail.com>
2024-02-13 08:18:44 -0600

    feat : Create step text and step circles

--- Multi-Step RegisterForm/index.js
+++ Multi-Step RegisterForm/index.js
@@ -2,8 +2,30 @@ const btnStep1 = document.querySelector("#submit-step1")
 const form1 = document.querySelector("#form-step1")
 const from2 = document.querySelector("#form-step2")
 
-btnStep1.addEventListener("click", (e) => {
-    e.preventDefault()
-    form1.classList.add("concluded")
-    from2.classList.add("current")
+const currentStepCircle = document.querySelectorAll(".circle");
+const currentStepText = document.querySelector(".footer__step");
+
+const forms = document.querySelectorAll(".main__form")
+const nameInputs = document.querySelectorAll(".input__name")
+const emailsInputs = document.querySelectorAll(".input__email")
+
+forms.forEach((form, formIdx) => {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+        validInputs(formIdx)
+    })
 })
+
+function validInputs(idx) {
+    const currentInputName = nameInputs[idx].value.trim()
+    const currentInputEmail = emailsInputs[idx].value.trim()
+    
+    if(currentInputName === "" || currentInputEmail === "") {
+        alert("Please complete the fileds")
+    } else {
+        forms[idx].classList.add("concluded")
+        currentStepCircle[idx+1].classList.add("active")
+        currentStepText.innerHTML = idx +2        
+        // forms[idx + 1].style.display = "block"
+    }
+}
