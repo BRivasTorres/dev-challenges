06117821c9e62c3aad4425eab46bee286b188779
Billy Torres <btorresr75@gmail.com>
2024-02-15 18:55:33 -0600

    feat : Finish second step form

--- Multi-Step RegisterForm/index.js
+++ Multi-Step RegisterForm/index.js
@@ -1,32 +1,54 @@
-const btnStep1 = document.querySelector("#submit-step1")
-const form1 = document.querySelector("#form-step1")
-const from2 = document.querySelector("#form-step2")
+const forms = document.querySelectorAll(".main__form");
+const nameInput = document.querySelector(".input__name");
+const emailInput = document.querySelector(".input__email");
+
+const btnStep1 = document.querySelector("#submit-step1");
+const form1 = document.querySelector("#form-step1");
+
+const form2 = document.querySelector(".form__step2");
+const topics = document.querySelectorAll(".step2__topic")
 
 const currentStepCircle = document.querySelectorAll(".circle");
 const currentStepText = document.querySelector(".footer__step");
+const btnStep2 = document.querySelector("#submit-step2");
 
-const forms = document.querySelectorAll(".main__form")
-const nameInputs = document.querySelectorAll(".input__name")
-const emailsInputs = document.querySelectorAll(".input__email")
+const summaryName = document.querySelector(".summary__name")
+const summaryEmail = document.querySelector(".summary__email")
+
+let topicSelected = "";
+let currForm = 1
 
 forms.forEach((form, formIdx) => {
-    form.addEventListener("submit", (e) => {
-        e.preventDefault()
-        validInputs(formIdx)
+	form.addEventListener("submit", (e) => {
+		e.preventDefault();
+        formIdx < 2 ? updateCurrentForm(formIdx) : ""
+	});
+});
+
+topics.forEach((topic, topicIdx) => {
+    topic.addEventListener("click", () => {
+        topicSelected = topic.innerHTML
+        topics.forEach((item, i) => {
+            item.classList.toggle("active-topic", i === topicIdx)
+        })
     })
 })
 
-function validInputs(idx) {
-    const currentInputName = nameInputs[idx].value.trim()
-    const currentInputEmail = emailsInputs[idx].value.trim()
-    
-    if(currentInputName === "" || currentInputEmail === "") {
-        alert("Please complete the fileds")
-    } else {
-        forms[idx].classList.add("form--concluded")
-        forms[idx+1].classList.remove("form--desactive")
-        currentStepCircle[idx+1].classList.add("active")
-        currentStepText.innerHTML = idx +2        
-        forms[idx + 1].style.display = "block"
-    }
+function updateCurrentForm(idx) {
+	if (idx === 1) {
+		printSummary();
+	}
+	currForm++;
+	forms[idx].classList.add("form--concluded");
+	forms[idx + 1].classList.remove("form--desactive");
+	currentStepCircle[idx + 1].classList.add("active");
+	currentStepText.innerHTML = currForm;
+	forms[idx + 1].style.display = "block";
+}
+//TODO resolver conflicto para renderizar los datos obtenidos en los formularios en sus etiquetas correspondientes
+
+function printSummary() {
+    summaryName.innerHTML = "fdafdafadfda"
+    summaryEmail.innerHTML = emailInput.innerHTML
+    console.log(summaryEmail, summaryName, emailInput.textContent)
 }
